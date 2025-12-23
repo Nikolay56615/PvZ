7598a58bf3883d855467bc678ceddd7c9801a534
Repeynik <M16072005m@gmail.com>
2025-12-23 18:40:43 +0700

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 app = FastAPI(title="IoT Backend", version="0.1.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://5.129.192.139:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://5.129.192.139:5173", "http://172.17.0.1:5173", "http://172.18.0.1:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
