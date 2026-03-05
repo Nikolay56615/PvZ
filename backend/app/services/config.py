621fa5113206116405d21be0d14da82b757c4705
Repeynik <M16072005m@gmail.com>
2026-03-06 03:30:25 +0700

    change allow origin

--- backend/app/services/config.py
+++ backend/app/services/config.py
@@ -1,5 +1,4 @@
 from typing import Any
-from pydantic import field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
@@ -24,20 +23,17 @@ class Settings(BaseSettings):
     dynsec_response_topic: str = "$CONTROL/dynamic-security/v1/response"
     dynsec_admin_username: str | None = None
     dynsec_admin_password: str | None = None
-
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
-
     
-    allow_origin: list[str] = ["http://localhost:5173"]
+    allow_origin: str = "http://localhost:5173"
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
     
-    @field_validator('allow_origin', mode='before')
-    @classmethod
-    def split_string_into_list(cls, v: Any) -> Any:
-        if isinstance(v, str):
-            return [item.strip() for item in v.split(',')]
-        return v
+    # @field_validator('allow_origin', mode='before')
+    # @classmethod
+    # def split_string_into_list(cls, v: Any) -> Any:
+    #     if isinstance(v, str):
+    #         return [item.strip() for item in v.split(',')]
+    #     return v
 
     @property
     def dsn(self) -> str:
